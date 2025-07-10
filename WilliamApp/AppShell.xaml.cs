af730b81b6cb3fbae0408b268a6a1e91cf93b4a3
AXEL YAIR MIRANDA <mirandaaxel17@gmail.com>
2025-07-10 09:01:23 -0300

    Agregar archivos de proyecto.

---
+++ WilliamApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WilliamApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WilliamApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WilliamApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
