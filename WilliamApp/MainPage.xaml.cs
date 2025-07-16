536b734eba202cbc7d7da80987e32d887ba477e1
AXEL YAIR MIRANDA <mirandaaxel17@gmail.com>
2025-07-16 08:44:51 -0300

    Limpieza

--- WilliamApp/MainPage.xaml.cs
+++ WilliamApp/MainPage.xaml.cs
@@ -9,19 +9,5 @@ namespace WilliamApp
         {
             InitializeComponent();
         }
-
-        [SupportedOSPlatform("windows10.0.17763.0")]
-        private async void OnVerReportesDeVentasClicked(object sender, EventArgs e)
-        {
-            if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 17763))
-            {
-                // Navegar a la página SalesReportPage
-                await Navigation.PushAsync(new SalesReportPage());
-            }
-            else
-            {
-                await DisplayAlert("Error", "Esta funcionalidad solo está disponible en Windows 10.0.17763.0 o versiones posteriores.", "OK");
-            }
-        }
     }
 }
