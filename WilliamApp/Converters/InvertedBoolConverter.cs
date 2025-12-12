493d03ea8720c5f72b626cd923717db9cc99f0a1
eliastinuk <eliastinuk@gmail.com>
2025-12-12 16:05:36 -0300

    cambio de logo

---
+++ WilliamApp/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace WilliamApp.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b ? !b : value;
+        }
+    }
+}
